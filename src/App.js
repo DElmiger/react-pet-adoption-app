d0151e898ec7593a20a2f9a272aee17b9f92a85d
“Dorothee <elmigerd@gmail.com>
2020-11-29 18:23:35 +0100

    moving pet to its own file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -19,8 +15,8 @@ const App = () => {
       animal: "Bird",
       breed: "Cockatiel",
     }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+    React.createElement(Pet, { name: "Joel", animal: "Cat", breed: "Mix" }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
